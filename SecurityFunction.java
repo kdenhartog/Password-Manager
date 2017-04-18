af9c716e99bed67e9fc4f3b27f5eb26ea7105c30
kdenhartog <kyle-denhartog@uiowa.edu>
2017-04-18 14:45:24 -0500

    minor changes including TODOs

--- SecurityFunction.java
+++ SecurityFunction.java
@@ -19,35 +19,42 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
  * @author kyle
  */
 public class SecurityFunction {
-    
-    public static byte[] hash(byte[] message) throws 
-            NoSuchAlgorithmException, 
+
+    public static byte[] hash(byte[] message) throws
+            NoSuchAlgorithmException,
             NoSuchProviderException{
         Security.addProvider(new BouncyCastleProvider());
-        
+
         MessageDigest mda = MessageDigest.getInstance("SHA-512", "BC");
         return mda.digest(message);
     }
-    
+
     //This uses AES-128/CTR/with Padding
-    public static byte[] encrypt() throws 
-            NoSuchAlgorithmException, 
-            NoSuchProviderException, 
+    public static byte[] encrypt() throws
+            NoSuchAlgorithmException,
+            NoSuchProviderException,
             NoSuchPaddingException {
         Security.addProvider(new BouncyCastleProvider());
-        
+
         Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding", "BC");
-        return null;
+        TODO: return null;
     }
-    
+
     public static byte[] saltGenerator() {
         Security.addProvider(new BouncyCastleProvider());
-            
+
             SecureRandom rand = new SecureRandom();
             byte[] salt = new byte[256];
             rand.nextBytes(salt);
             return salt;
-            
+
     }
-    
+
+    public static byte[] generateKey(byte[] master_pass) {
+      Security.addProvider(new BouncyCastleProvider());
+
+      //will return a byte[] key based upon master_pass using PKCS5_PBKDFT_HMAC
+      return null;
+    }
+
 }
