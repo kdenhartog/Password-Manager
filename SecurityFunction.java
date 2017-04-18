d83085fa19ba7138e8673d93d0b03c36032012a9
NicholasKao <kao.nicholas@gmail.com>
2017-04-18 18:58:19 -0500

    Add files via upload

--- SecurityFunction.java
+++ SecurityFunction.java
@@ -5,13 +5,18 @@
  */
 package passwordmanager;
 
+import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.security.Security;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 /**
@@ -30,27 +35,54 @@ public class SecurityFunction {
     }
 
     //This uses AES-128/CTR/with Padding
-    public static byte[] encrypt() throws
+    public static byte[] encrypt(byte[] data) throws
             NoSuchAlgorithmException,
             NoSuchProviderException,
-            NoSuchPaddingException {
+            NoSuchPaddingException,
+            InvalidKeyException,
+            IllegalBlockSizeException,
+            BadPaddingException {
+        Security.addProvider(new BouncyCastleProvider());
+        
+        //creating key and cipher
+        byte[] key = generateKey();
+        Key aesKey = new SecretKeySpec(key, "AES/CTR/NoPadding");
+        Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding", "BC");
+        
+        //encrypting
+        cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+        byte[] encrypted = cipher.doFinal(data);
+        
+       return encrypted;
+    }
+    
+    public static byte[] decrypt(byte[] encrypted) throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
+        
         Security.addProvider(new BouncyCastleProvider());
 
+        //creating key and cipher
+        byte[] key = generateKey();
+        Key aesKey = new SecretKeySpec(key, "AES/CTR/NoPadding");
         Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding", "BC");
-        TODO: return null;
+        
+        //decrypting
+        cipher.init(Cipher.DECRYPT_MODE, aesKey);
+        byte[] decrypted = cipher.doFinal(encrypted);
+        
+        return decrypted;
     }
 
-    public static byte[] saltGenerator() {
+    public static byte[] randomNumberGenerator() {
         Security.addProvider(new BouncyCastleProvider());
 
             SecureRandom rand = new SecureRandom();
-            byte[] salt = new byte[256];
-            rand.nextBytes(salt);
-            return salt;
+            byte[] data = new byte[256];
+            rand.nextBytes(data);
+            return data;
 
     }
 
-    public static byte[] generateKey(byte[] master_pass) {
+    public static byte[] generateKey() {
       Security.addProvider(new BouncyCastleProvider());
 
       //will return a byte[] key based upon master_pass using PKCS5_PBKDFT_HMAC
